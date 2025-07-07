7e93db1988d2da52e94c6d1d3bf146743de3aa9c
PotterIle <elindeda1@gmail.com>
2025-07-07 16:47:47 +0300

    factorial

---
+++ RecursiveFunctions/Sum.java
@@ -0,0 +1,18 @@
+package RecursiveFunctions;
+
+public class Sum {
+    public static int sum(int n) {
+        //Base case: if n is 1, the sum is 1
+        if (n==1) {
+            return 1;
+        } else {
+            //Recursive case: n + sum of n - 1
+            return n + sum(n-1);
+        }
+    
+    }
+        public static void main(String[] args) {
+        int result = sum(5);    //calculates the sum of numbers 1 to 5
+        System.out.println("Sum " + result); //Output sum:15
+    }
+}
